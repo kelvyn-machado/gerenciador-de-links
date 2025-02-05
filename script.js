c8716d92879b88b2b16fa4614af76a1934bd106a
Kelvyn Machado <kelvynmachado23@hotmail.com>
2025-02-05 01:06:17 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  /*if (html.classList.contains('dark')) {
+    html.classList.remove('dark')
+  } else {
+    html.classList.add('dark')
+  }*/
+
+    html.classList.toggle('dark')
+}
